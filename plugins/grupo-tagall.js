822d1582c441756cbe5e8f4009eb885fbdc16a5e
Ayra-08 <160386340+Ayra-08@users.noreply.github.com>
2024-05-11 23:57:24 -0500

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -10,9 +10,9 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `${pesan}`;
-  let teks = `*Igna Bot*\n\n *Integrantes :  ${participants.length}* ${oi}\n\n┌──⭓ Ya revivan\n`;
+  let teks = `*Haru Bot*\n\n *Integrantes :  ${participants.length}* ${oi}\n\n┌──⭓ Ya revivan\n`;
   for (const mem of participants) {
-    teks += `│⭔ @${mem.id.split('@')[0]}\n`;
+    teks += `☁  @${mem.id.split('@')[0]}\n`;
   }
   teks += `└───────⭓
 
